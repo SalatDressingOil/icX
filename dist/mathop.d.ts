3fee6149b9719dac26d0f76163f411abe18850ef
Kirill Nefediev <traineratwot@yandex.ru>
2021-05-10 19:22:20 +0300

    clear

--- dist/mathop.d.ts
+++
@@ -1,9 +0,0 @@
-import { icXElem } from "./classes";
-export declare class icXIncrement extends icXElem {
-    constructor(scope: icXElem | null, pos?: number, text?: string);
-    compile(): string | null;
-}
-export declare class icXDecrement extends icXElem {
-    constructor(scope: icXElem | null, pos?: number, text?: string);
-    compile(): string | null;
-}
--- dist/modules/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { icXElem } from "../classes";
-declare const modules: {
-    [smile: string]: icXElem;
-};
-export default modules;
